2c4b63af840d65eebff793dbe89a9f8273731172
Aiyaruch Phengthong <aiyaruch1320@hotmail.com>
2024-02-10 20:53:32 +0700

    Add ThemeSwitcher component to dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,12 @@
+import ThemeSwitcher from "@/components/theme-switcher";
 import React from "react";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
-      <nav>Nav</nav>
+      <nav>
+        <ThemeSwitcher />
+      </nav>
       <main className="flex w-full flex-grow">{children}</main>
     </div>
   );

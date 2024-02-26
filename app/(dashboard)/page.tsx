f20d5db133fdc9c7ddbe549b3a49b05d9cca150c
Aiyaruch Phengthong <aiyaruch1320@hotmail.com>
2024-02-26 10:08:41 +0700

    Add CreateFormBtn component

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,4 +1,5 @@
 import { GetFormStats } from "@/actions/form";
+import CreateFormBtn from "@/components/create-form-btn";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
@@ -17,6 +18,7 @@ export default function Home() {
       <Separator className="my-6" />
       <h2 className="text-4xl font-bold col-span-2">Your forms</h2>
       <Separator className="my-6" />
+      <CreateFormBtn />
     </div>
   );
 }
---
+++ components/create-form-btn.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+
+function CreateFormBtn() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Create new form</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create form</DialogTitle>
+          <DialogDescription>
+            Create a new form to start collecting responses
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default CreateFormBtn;

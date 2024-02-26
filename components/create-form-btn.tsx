dc98ef2a394e7aca1b93cbf6ab93e113d5089d4d
Aiyaruch Phengthong <aiyaruch1320@hotmail.com>
2024-02-26 11:43:39 +0700

    Add form validation and submission functionality to CreateFormBtn component

--- components/create-form-btn.tsx
+++ components/create-form-btn.tsx
@@ -1,16 +1,44 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { ImSpinner2 } from "react-icons/im";
+import * as z from "zod";
 import { Button } from "./ui/button";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "./ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+
+const formSchema = z.object({
+  name: z.string().min(4),
+  description: z.string().optional(),
+});
+
+type formSchemaType = z.infer<typeof formSchema>;
 
 function CreateFormBtn() {
+  const form = useForm<formSchemaType>({
+    resolver: zodResolver(formSchema),
+  });
+  function onSubmit(values: formSchemaType) {
+    console.log(values);
+  }
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -23,6 +51,49 @@ function CreateFormBtn() {
             Create a new form to start collecting responses
           </DialogDescription>
         </DialogHeader>
+        <Form {...form}>
+          <form className="space-y-2">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea rows={5} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+        <DialogFooter>
+          <Button
+            onClick={form.handleSubmit(onSubmit)}
+            disabled={form.formState.isSubmitting}
+            className="w-full mt-4"
+          >
+            {form.formState.isSubmitting ? (
+              <ImSpinner2 className="animet-spin" />
+            ) : (
+              "Save"
+            )}
+          </Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );

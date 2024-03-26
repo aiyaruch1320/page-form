21db590a19f70842372aefc12b21febc8ce75b54
Aiyaruch <aiyaruch1320@hotmail.com>
2024-03-26 21:06:14 +0700

    Fix class name in CreateFormBtn component

--- components/create-form-btn.tsx
+++ components/create-form-btn.tsx
@@ -55,7 +55,7 @@ function CreateFormBtn() {
           variant={"outline"}
           className="group border border-primary/20 h-[198px] items-center justify-center flex flex-col hover:border-primary hover:cursor-pointer border-dashed gap-4"
         >
-          <BsFileEarmarkPlus className="h-8 w-8 to-muted-foreground group-hover:text-primary" />
+          <BsFileEarmarkPlus className="h-8 w-8 text-muted-foreground group-hover:text-primary" />
           <p className="font-bold text-xl text-muted-foreground group-hover:text-primary">
             Create new form
           </p>

b063b79a18bb2e86700f5b5cb83c0238a2ead97e
Aiyaruch Phengthong <aiyaruch1320@hotmail.com>
2024-04-24 15:23:09 +0700

    Add designer component

--- components/form-builder.tsx
+++ components/form-builder.tsx
@@ -5,27 +5,33 @@ import React from "react";
 import PreviewDialogBtn from "./preview-dialog-btn";
 import SaveFormBtn from "./save-form-btn";
 import PublishFormBtn from "./publish-form-btn";
+import Designer from "./designer";
+import { DndContext } from "@dnd-kit/core";
 
 function FormBuilder({ form }: { form: Form }) {
   return (
-    <main className="flex flex-col w-full">
-      <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
-        <h2 className="truncate font-medium">
-          <span className="text-muted-foreground mr-2">Form:</span>
-          {form.name}
-        </h2>
-        <div className="flex items-center gap-2">
-          <PreviewDialogBtn />
-          {!form.published && (
-            <>
-              <SaveFormBtn />
-              <PublishFormBtn />
-            </>
-          )}
+    <DndContext>
+      <main className="flex flex-col w-full">
+        <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
+          <h2 className="truncate font-medium">
+            <span className="text-muted-foreground mr-2">Form:</span>
+            {form.name}
+          </h2>
+          <div className="flex items-center gap-2">
+            <PreviewDialogBtn />
+            {!form.published && (
+              <>
+                <SaveFormBtn />
+                <PublishFormBtn />
+              </>
+            )}
+          </div>
+        </nav>
+        <div className="flex w-full flex-grow items-center justify-center relative overflow-y-auto h-[200px] bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
+          <Designer />
         </div>
-      </nav>
-      <div className="flex w-full flex-grow items-center justify-center relative overflow-y-auto h-[200px] bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]"></div>
-    </main>
+      </main>
+    </DndContext>
   );
 }
 

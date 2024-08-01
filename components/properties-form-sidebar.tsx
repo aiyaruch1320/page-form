1fa508069a2b737bfbea98bb2de375b8e3f6c086
Aiyaruch <aiyaruch1320@hotmail.com>
2024-08-01 23:14:48 +0700

    Display PropertiesForm when click designer element

--- components/properties-form-sidebar.tsx
+++ components/properties-form-sidebar.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import useDesigner from "./hooks/use-designer";
+import { FormElements } from "./form-elements";
 
 function PropertiesFormSidebar() {
-  return <div></div>;
+  const { selectedElement } = useDesigner();
+  if (!selectedElement) return null;
+
+  const PropertiesForm = FormElements[selectedElement.type].propertiesComponent;
+  return <PropertiesForm />;
 }
 
 export default PropertiesFormSidebar;
